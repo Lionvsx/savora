bed56be741667e724ae180226dd56a65a8c0f589
Leonard Roussard <lroussard@gmail.com>
2025-01-20 13:05:57 +0100

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/sonner";
 
 const FONT_SANS = DM_Sans({
@@ -22,11 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${FONT_SANS.className} antialiased bg-gray-50`}
+        className={`${FONT_SANS.className} antialiased bg-gray-50 scroll-smooth`}
       >
         <ThemeProvider forcedTheme="light" defaultTheme="light">
           <Toaster richColors position="top-center" theme="light" />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
